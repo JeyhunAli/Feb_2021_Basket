a4fb5a31aab8164fb97a3a493ec4f54af1986a12
Jey <jey@Jeys-MacBook-Pro.local>
2021-05-29 16:01:33 -0400

    new classes plz have a look

--- src/main/java/selenium_concept/BrowserWindowPopUpHandle.java
+++ src/main/java/selenium_concept/BrowserWindowPopUpHandle.java
@@ -12,16 +12,33 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BrowserWindowPopUpHandle {
 	
 	/**
+	 * cross origin policy new window pop up concept 
+	 * one thing selenium supports window pop like cross origin pop up but cyprus not
+	 * selenium handle it by using 
+	 * window handler api
+	 * always we need window id to switch between window
+	 * now another big concept comes into the picture exact way to use 
 	 * how will u handle window pop up 
 	 * we can handle window pop up i mean multiple window handling 
-	 * with windowHandles api there is a method getWindowHandles which will return the set object
+	 * with windowHandler api there is a method getWindowHandles which will return the set object
 	 * and i will iterate the set object to get window id one by one
-	 * why iterator because set object is orderless
-	 * and the using switch to window method and passing child window id 
+	 * why iterator because iterator applied on set object and set is orderless
+	 * and then using switch to window method and passing child window id 
+	 * and what ever we want to verify on child window sunch as getting window title current url 
 	 * as selenium supports cross origin policy thats why we can switch between window 
 	 * once i switch tp child window i can do some verification such getting page title or current url 
-	 * once im done with child window i have to close it not quit because quit will close netire session 
+	 * once im done with child window i have to close it not quit because quit will close entire session 
 	 * then switching back to the parent window then here i can quit or close 
+	 * 
+	 * 
+	 * driver.getWindowHandle()  and driver.getWindowHandles() 
+	 * differences between them first one is for single window and it will tell you where is your window and it can provide you that window id 
+	 * second one is for hansling multiple window 
+	 * 
+	 *  another concept selenium is not able to handle random pop up 
+	 *  which is we dont know when it comes thats why while developing testing we always asking developer for timely to disable them 
+	 *  this is limitations of selenium
+	 * 
 	 * @param args
 	 */
 
@@ -38,12 +55,12 @@ public class BrowserWindowPopUpHandle {
 
 		Iterator<String> it = handles.iterator();
 
-		while (it.hasNext()) {
-			String id = it.next();
-			driver.switchTo().window(id);
-			System.out.println(driver.getTitle());
-			driver.close();
-		}
+//		while (it.hasNext()) {
+//			String id = it.next();
+//			driver.switchTo().window(id);
+//			System.out.println(driver.getTitle());
+//			driver.close();
+//		}
 
 		String parentWindowId = it.next();
 		System.out.println("Parent window id is: " + parentWindowId);
---
+++ src/main/java/selenium_concept/CustomXpath_1.java
@@ -0,0 +1,79 @@
+package selenium_concept;
+
+public class CustomXpath_1 {
+
+	public static void main(String[] args) {
+
+		//Xpath:
+		//1. Absolute Xpath : //html/body/div[3]/div/div[]/ul/li/a   complete adress of the html dom
+		//html/body/div[2]/div/div/div/div[1]/div/h2
+		
+		//2. Relative Xpath or custom xpath: 
+		//input
+		//input[@attr]
+		//htmtag[@attr='value']
+		//input[@id='input-email']
+		//input[@name='email']
+		
+		//htmltag[@attr1='value' and @attr2='value']
+		//input[@name='email' and @type='text']           more than one attr  and means both the attr have to be there 
+		//input[@name='email' or @type='text']            or means either of them  has to be there mostly we use and operator to get exact element
+
+		//input[@value='Login' and @type='submit']
+		
+		//input[@type='text'] -- 3
+		//*[@type='text'] -- 3           * means it will check all the html tag inside dom
+		
+		//text() function:       is used for to get specific text of the element 
+		//h2[text()='Refreshing business software that your teams will love']
+		//a[text()='Login']
+		
+		//contains() function:    
+		//htmltag[contains(@attr,'value')]
+		//input[contains(@name,'username')]
+		//input[contains(@name,'user')]
+		//input[contains(@name,'name')]
+		
+		//htmltag[contains(@attr1,'value') and contains(@attr2='value')]
+		//input[contains(@name,'name') and contains(@type,'text')]
+		
+		//contains() with text():
+		//htmltag[contains(text(),'value')]
+		//h2[contains(text(),'Refreshing')]
+		//h2[contains(text(),'business software')]
+		
+		//contains() with text() and attribute:
+		//htmltag[contains(text(),'value') and contains(@attr,'value')]
+		//a[contains(text(),'Gift Cards') and contains(@data-csa-c-slot-id,'nav_cs_3')]
+		//a[contains(text(),'About Us') and contains(@href,'aboutamazon')]
+		
+		//contains with links, span, p, small
+		//small[contains(text(),'tracking tool and sales pipeline')]
+		
+		//String xpath = "//small[contains(text(),\"tracking tool and sales pipeline\")]";
+		
+		//position() function:
+		// (//input[@type='text'])[position()=1]
+		// (//input[@class='form-control'])[position()=1]
+		// (//input[@class='form-control'])[1]
+		// (//ul[@class='footer-nav'])[1]//a
+		
+		//last():
+		// (//input[@class='form-control'])[last()]
+		
+		//dynamic ids or any attribute:
+//		<input id="username_123">
+//		<input id="username_345">
+//		<input id="username_567">
+		//EXT JS 
+		//input[contains(@id,'username_')]
+		
+		//starts-with():
+		// (//div[starts-with(@class,'navFooterLinkCol')])
+		//input[starts-with(@id,'username_')]
+
+		
+		
+	}
+
+}
